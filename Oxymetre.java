f30a710caf523ca76236117c24521c55d231a5f7
Merouani Souha <souha@Merouanis-MacBook-Air.local>
2025-11-13 12:00:50 +0100

    lajout des class et explication

---
+++ Oxymetre.java
@@ -0,0 +1,20 @@
+public class Oxymetre extends CapteurConnecte{
+    private double spo2; // saturation en oxygène
+
+    public Oxymetre(String id, String nom) {    
+    super(id, nom);
+    }
+
+    public void mesurer(){
+        spo2= 88 + Math.random()*12;
+        valeur=spo2;
+    }
+
+    public boolean verifierAlerte() {
+        return spo2 < 92; 
+    }
+    public String toString() {
+    return super.toString() + String.format(" [SpO2=%.1f%%]", spo2);
+    }
+
+}
